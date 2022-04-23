eab3ec38e9bcb8074f950bad33aa9bedbaaaabe3
Samuel John <opeoniye@gmai.com>
2022-04-23 21:03:17 +0100

    kubernetes ci/cd project

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=<username>
+export POSTGRES_PASSWORD=<password>
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=<postgres-db>
+export AWS_BUCKET=<s3-bucket>
+export AWS_REGION=us-east-1
+export AWS_PROFILE=<aws-profile>
+export JWT_SECRET=<secret>
+export URL=<url>
